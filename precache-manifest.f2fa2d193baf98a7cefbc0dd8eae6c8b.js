bf058f9c0418a8ed23a307fe37bc9b3c68e39176
assept <65564708@qq.com>
2019-09-15 12:52:46 +0800

    Updates

---
+++ precache-manifest.f2fa2d193baf98a7cefbc0dd8eae6c8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68041d7eaa5a31e4a5f68264eb511d65",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "52d07c79dbf966a86101",
+    "url": "/rolodex/static/css/main.02dda7a7.chunk.css"
+  },
+  {
+    "revision": "2ee468953aa86f790865",
+    "url": "/rolodex/static/js/2.dee2eb05.chunk.js"
+  },
+  {
+    "revision": "52d07c79dbf966a86101",
+    "url": "/rolodex/static/js/main.2d4b97ed.chunk.js"
+  },
+  {
+    "revision": "93ef8cb23bbabfa7d434",
+    "url": "/rolodex/static/js/runtime~main.687a14ef.js"
+  }
+]);
